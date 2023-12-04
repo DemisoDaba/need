a8fb82eaa81e197f7f7e96889b02eb5bc0d5e6b1
Demisodaba <demo.nkmt1@gmail.com>
2023-12-04 11:36:09 +0300

    done

---
+++ README.md
@@ -0,0 +1,3 @@
+# Portfolio
+Portfolio Project for ALX-Africa School.
+Access it from [here](hhh)
---
+++ script.js
@@ -0,0 +1,13 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.feature, .feature1');
+hiddenElements.forEach((el) => observer.observe(el));
